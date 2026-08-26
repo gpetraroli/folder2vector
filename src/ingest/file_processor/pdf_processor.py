cc4d925b5c6bd1d03b3ff9a24d0f6be24de01009
g.petraroli <g.petraroli@gmail.com>
2026-08-26 08:49:35 +0200

    Add PDFProcessor for handling PDF documents and update dependencies

---
+++ src/ingest/file_processor/pdf_processor.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from folder2vector.config import CHUNK_OVERLAP, CHUNK_SIZE
+
+from .file_processor_interface import FileProcessorInterface
+
+
+class PDFProcessor(FileProcessorInterface):
+    def __str__(self) -> str:
+        return "PDFProcessor"
+
+    def process(self, file_path: str) -> None:
+        splits = self.split_pdf_document(file_path)
+
+        self.embed_documents(file_path, splits)
+
+    def split_pdf_document(self, file_path: str) -> list[Document]:
+        loader = PyPDFLoader(file_path)
+        docs = [doc for doc in loader.load() if doc.page_content.strip()]
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=CHUNK_SIZE,
+            chunk_overlap=CHUNK_OVERLAP,
+        )
+        return text_splitter.split_documents(docs)
+        
